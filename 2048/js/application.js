1a93a54ae6f0bc82411cb5f32174d486fb366efe
657994434 <657994434@qq.com>
2017-02-18 19:20:10 +0800

    Site updated: 2017-02-18 19:20:10

---
+++ 2048/js/application.js
@@ -0,0 +1,4 @@
+<script src="/assets/js/APlayer.min.js"> </script>// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
